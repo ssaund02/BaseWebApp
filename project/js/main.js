fd1d8b0a36b91b11bc93f76aae4f96bb28770bf0
ssaund02 <sophie.saunders@tufts.edu>
2019-08-19 21:47:24 -0400

    Added Boston weather API

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+	getWeather();
+})
+
+function getWeather() {
+	var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&APPID="+apiKey;
+	
+	$.ajax(url,{success: function(data){
+		$(".city").text(data.name);
+		$(".temp").text(data.main.temp);
+	}})
+}
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
